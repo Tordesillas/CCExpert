2029be4070ead0a2e5a67433d12672601f90b066
Tordesillas <guillaume.casagrande@etu.unice.fr>
2017-09-07 12:51:11 +0200

    Fix du calcul de la précision

--- src/fr/polytech/ccexpert/controller/DodgeProcessor.java
+++ src/fr/polytech/ccexpert/controller/DodgeProcessor.java
@@ -1,8 +1,8 @@
 package fr.polytech.ccexpert.controller;
 
+import com.codename1.components.SpanLabel;
 import com.codename1.l10n.L10NManager;
 import com.codename1.ui.Container;
-import com.codename1.ui.Label;
 
 public class DodgeProcessor {
     private L10NManager l10n;
@@ -15,10 +15,11 @@ public class DodgeProcessor {
         Container c = new Container();
         double amountsTalent[] = {0.05, 0.06, 0.07, 0.08, 0.09, 0.15, 0.2, 0.3};
         double amountTalent = (talentBool) ? amountsTalent[talentLvl-1] : 0;
-        double amountArtefact = (artefactBool) ? 1 : 0;
+        double amountArtefact = (artefactBool) ? 0.15 : 0;
         double amount = amountTalent + (1-amountTalent)*amountArtefact + (1-amountTalent)*(1-amountArtefact)*(basic+extra)/10000;
 
-        c.addComponent(new Label("Esquive totale : " + l10n.format(amount)));
+        c.addComponent(new SpanLabel("Le héros sera capable d'esquiver " + l10n.format(amount*100) + "% des attaques adverses.\n" +
+                "Il esquivera donc environ " + l10n.format(Math.round(amount*10)) +" coups sur 10."));
 
         return c;
     }
